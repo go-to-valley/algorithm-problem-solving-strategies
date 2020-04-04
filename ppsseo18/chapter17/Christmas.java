a6871f172f08bcabf13a813c4d989b02414d4f72
ppsseo18 <ppsseo18@gmail.com>
2020-04-04 17:51:42 +0900

    Add problems of chapter 17

---
+++ ppsseo18/chapter17/Christmas.java
@@ -0,0 +1,107 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public class Christmas {
+	public static class TestCase {
+		int numberOfGitfBoxes;
+		int numberOfChildren;
+		List<Integer> partialSumListOfGiftBoxes;
+
+		Map<Integer, List<Integer>> possibleIndexPairMap = new HashMap<Integer, List<Integer>>();
+
+		int answerOfFirstQuestion;
+		int answerOfSecondQuestion;
+
+		public TestCase(String[] firstLineTokens, String[] secondLineTokens) {
+			this.numberOfGitfBoxes = Integer.valueOf(firstLineTokens[0]);
+			this.numberOfChildren = Integer.valueOf(firstLineTokens[1]);
+
+			// calculate and save partial sums
+			this.partialSumListOfGiftBoxes = new ArrayList<Integer>(this.numberOfGitfBoxes);
+			for (int i = 0; i < secondLineTokens.length; i++) {
+				int parsedToken = Integer.valueOf(secondLineTokens[i]);
+				this.partialSumListOfGiftBoxes.add(i == 0 ? parsedToken : parsedToken + partialSumListOfGiftBoxes.get(i - 1));
+			}
+
+			answerOfFirstQuestion = 0;
+			answerOfSecondQuestion = 0;
+		}
+
+		public void resolveFirstQuestion() {
+			for (int i = 0; i < numberOfGitfBoxes; i++) {
+				for (int j = i; j < numberOfGitfBoxes; j++) {
+					int partialSum = i == 0 ? partialSumListOfGiftBoxes.get(j)
+							: partialSumListOfGiftBoxes.get(j) - partialSumListOfGiftBoxes.get(i - 1);
+					boolean isSatisfied = testDoesPartialSumSatisfyCondition(partialSum);
+					if (isSatisfied) {
+						if (possibleIndexPairMap.get(i) == null) {
+							possibleIndexPairMap.put(i, new ArrayList<Integer>());
+						}
+						possibleIndexPairMap.get(i).add(j);
+						answerOfFirstQuestion++;
+					}
+				}
+			}
+		}
+
+		public void resolveSecondQuestion() {
+			List<Integer> firstList = null;
+			for (int i = 0; i < numberOfGitfBoxes; i++) {
+				firstList = possibleIndexPairMap.get(i);
+				if (firstList != null) {
+					answerOfSecondQuestion++;
+					break;
+				}
+			}
+			this.DFS(firstList, answerOfSecondQuestion);
+		}
+
+		private void DFS(List<Integer> list, int depth) {
+			for (int index : list) {
+				List<Integer> next = possibleIndexPairMap.get(index);
+				if (next == null) {
+					continue;
+				} else {
+					DFS(next, depth + 1);
+				}
+			}
+			if (depth > answerOfSecondQuestion) {
+				answerOfSecondQuestion = depth;
+			}
+		}
+
+		private boolean testDoesPartialSumSatisfyCondition(int partialSum) {
+			return partialSum % numberOfChildren == 0;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		int numberOfTestCase;
+		List<TestCase> testCaseList = new ArrayList<TestCase>();
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+
+		numberOfTestCase = Integer.valueOf(stdin.readLine());
+		for (int t = 0; t < numberOfTestCase; t++) {
+
+			String[] firstLineTokens = stdin.readLine().split("\\s");
+			String[] secondLineTokens = stdin.readLine().split("\\s");
+			testCaseList.add(new TestCase(firstLineTokens, secondLineTokens));
+		}
+
+		for (TestCase testCase : testCaseList) {
+
+			testCase.resolveFirstQuestion();
+			testCase.resolveSecondQuestion();
+
+			System.out.print(Integer.toString(testCase.answerOfFirstQuestion) + " "
+					+ Integer.toString(testCase.answerOfSecondQuestion) + "\n");
+		}
+	}
+
+}
